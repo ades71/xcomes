e12a7d8c401ce450ad40d4f85048fd5c5f4b4b68
ades71 <teary71@naver.com>
2024-08-07 20:42:45 +0900

    settings 수정

--- setting/urls.py
+++ setting/urls.py
@@ -7,7 +7,7 @@ from django.urls import path,  include
 # from rest_framework_swagger.views import get_swagger_view
 
 urlpatterns = [
-    path("admin", admin.site.urls),
+    path("admin/", admin.site.urls),
     path('users/', include('authentication.api.urls'), name='authentication'),
     # url(r'^rest-auth/', include('rest_auth.urls')),
     # url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
